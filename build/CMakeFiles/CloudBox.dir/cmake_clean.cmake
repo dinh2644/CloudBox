d315c77a83f242a2fe6c333e9e06eb45a180e077
dinh2644 <dinh2644.mail@gmail.com>
2025-06-11 00:42:26 -0400

    simple cmake builds src/main setup

---
+++ build/CMakeFiles/CloudBox.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CloudBox.dir/src/main.cpp.o"
+  "CMakeFiles/CloudBox.dir/src/main.cpp.o.d"
+  "CloudBox"
+  "CloudBox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CloudBox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CloudBox.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CloudBox.
+# This may be replaced when dependencies are built.
